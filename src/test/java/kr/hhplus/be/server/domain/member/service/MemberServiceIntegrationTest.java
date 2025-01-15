a65a134be20b608f495eab206342104a5701ec41
nashs789 <bbock1214@gmail.com>
2025-01-16 00:36:43 +0900

    🧪Test: 포인트 API 통합 테스트 작성(이전 과제)

--- src/test/java/kr/hhplus/be/server/domain/member/service/MemberServiceIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/domain/member/service/MemberServiceIntegrationTest.java
@@ -1,23 +1,33 @@
 package kr.hhplus.be.server.domain.member.service;
 
-import kr.hhplus.be.server.domain.member.repository.MemberRepository;
+import kr.hhplus.be.server.domain.member.command.PointChargeCommand;
+import kr.hhplus.be.server.domain.member.command.PointUseCommand;
+import kr.hhplus.be.server.domain.member.exception.PointException;
+import kr.hhplus.be.server.domain.member.info.MemberInfo;
 import kr.hhplus.be.server.infra.member.entity.Member;
 import kr.hhplus.be.server.infra.member.repository.MemberJpaRepository;
-import kr.hhplus.be.server.infra.member.repository.impl.MemberRepositoryImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-// TODO - 통합 테스트 작성 해야하는데 오류 해결 해야겠는데...?
+import static kr.hhplus.be.server.infra.member.entity.PointHistory.PointUseType.CHARGE;
+import static kr.hhplus.be.server.infra.member.entity.PointHistory.PointUseType.USE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @SpringBootTest
+@Testcontainers
 class MemberServiceIntegrationTest {
 
+    private final Long BASE_POINT = 100_000L;
     private Member member;
 
     @Autowired
@@ -29,27 +39,96 @@ class MemberServiceIntegrationTest {
     @BeforeEach
     void setUp() {
         member = memberJpaRepository.save(Member.builder()
-                                                .point(10_000L)
+                                                .point(BASE_POINT)
                                                 .build());
     }
 
     @Test
     @DisplayName("동시 충전 동시성 테스트")
-    void chargeMemberPointConcurrencyTest() {
-    }
+    void chargeMemberPointConcurrencyTest() throws InterruptedException {
+        // given
+        final int TEST_CNT = 30;
+        final long CHARGE_POINT = 10_000L;
+        ExecutorService executor = Executors.newFixedThreadPool(30);
+        CountDownLatch latch = new CountDownLatch(TEST_CNT);
 
-    @Test
-    @DisplayName("포인트 충전 실패")
-    void failChargeMemberPoint() {
+        // when
+        for(int i = 0; i < TEST_CNT; i++) {
+            executor.submit(() -> {
+                try {
+                    memberService.chargeMemberPoint(PointChargeCommand.builder()
+                                                                      .memberId(member.getId())
+                                                                      .chargePoint(CHARGE_POINT)
+                                                                      .pointUseType(CHARGE)
+                                                                      .build());
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        MemberInfo result = memberService.findMemberById(member.getId());
+
+        // then
+        final Long FINAL_POINT = BASE_POINT + TEST_CNT * CHARGE_POINT;
+
+        assertEquals(FINAL_POINT, result.getPoint());
+        assertEquals(0, latch.getCount());
     }
 
     @Test
     @DisplayName("동시 사용 동시성 테스트")
-    void useMemberPointConcurrencyTest() {
+    void useMemberPointConcurrencyTest() throws InterruptedException {
+        // given
+        final int TEST_CNT = 10;
+        final long USE_POINT = 10_000L;
+        ExecutorService executor = Executors.newFixedThreadPool(30);
+        CountDownLatch latch = new CountDownLatch(TEST_CNT);
+
+        // when
+        for(int i = 0; i < TEST_CNT; i++) {
+            executor.submit(() -> {
+                try {
+                    memberService.useMemberPoint(PointUseCommand.builder()
+                                                                .memberId(member.getId())
+                                                                .usePoint(USE_POINT)
+                                                                .pointUseType(USE)
+                                                                .build());
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        MemberInfo result = memberService.findMemberById(member.getId());
+
+        // then
+        assertEquals(0L, result.getPoint());
+        assertEquals(0, latch.getCount());
     }
 
     @Test
     @DisplayName("포인트 사용 실패")
     void failUseMemberPoint() {
+        // given
+        final Long USE_POINT = BASE_POINT + 1L;
+
+        // when
+        PointException pointException = assertThrows(PointException.class, () ->
+                memberService.useMemberPoint(PointUseCommand.builder()
+                                                            .memberId(member.getId())
+                                                            .usePoint(USE_POINT)
+                                                            .pointUseType(USE)
+                                                            .build())
+        );
+
+        // then
+        assertEquals(PointException.class, pointException.getClass());
+        assertEquals(BAD_REQUEST, pointException.getStatus());
+        assertEquals("포인트가 부족 합니다.", pointException.getMessage());
     }
 }
