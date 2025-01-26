03b949a03bd396ead35ac1c4ec74d8466cdc8bf5
nashs789 <bbock1214@gmail.com>
2025-01-26 17:31:54 +0900

    ❌Del: 필요 없는 테스트 파일 삭제

--- src/test/java/kr/hhplus/be/server/application/payment/facade/PaymentFacadeTest.java
+++ src/test/java/kr/hhplus/be/server/application/payment/facade/PaymentFacadeTest.java
@@ -1,7 +1,12 @@
 package kr.hhplus.be.server.application.payment.facade;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Testcontainers
 class PaymentFacadeTest {
 
 }
